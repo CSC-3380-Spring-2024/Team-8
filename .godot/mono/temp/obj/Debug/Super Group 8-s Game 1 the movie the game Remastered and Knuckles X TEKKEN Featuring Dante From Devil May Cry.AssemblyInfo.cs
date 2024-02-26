817990e2dfac0e31fda9c0e9ad0257f536e69cdc
hallowann <sophieanngs@gmail.com>
2024-02-26 12:14:40 -0600

    goodbye knuckles :(

--- .godot/mono/temp/obj/Debug/Super Group 8-s Game 1 the movie the game Remastered and Knuckles X TEKKEN Featuring Dante From Devil May Cry.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Super Group 8-s Game 1 the movie the game Remastered and Knuckles X TEKKEN Featuring Dante From Devil May Cry.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
     "ing Dante From Devil May Cry")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+520ee369712715f4eb45ebbcc97d65a4456ffbd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87877ffe11ca417e9d41cb7452bb257d90cf06a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Super Group 8-s Game 1 the movie the game Remastered and Knuckles X TEKKEN Featur" +
     "ing Dante From Devil May Cry")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Super Group 8-s Game 1 the movie the game Remastered and Knuckles X TEKKEN Featur" +
