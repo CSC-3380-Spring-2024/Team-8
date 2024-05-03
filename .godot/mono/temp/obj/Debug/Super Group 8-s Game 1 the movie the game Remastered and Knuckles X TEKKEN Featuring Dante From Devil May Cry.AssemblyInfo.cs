cfa18d6eae7e0b6f37cef0bf179be343745f9d71
hallowann <sophieanngs@gmail.com>
2024-05-03 01:42:27 -0500

    functional title screen!

--- .godot/mono/temp/obj/Debug/Super Group 8-s Game 1 the movie the game Remastered and Knuckles X TEKKEN Featuring Dante From Devil May Cry.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Super Group 8-s Game 1 the movie the game Remastered and Knuckles X TEKKEN Featuring Dante From Devil May Cry.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
     "ing Dante From Devil May Cry")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bd8f482f1fcdb94863c210bad9a9f848f01d1bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a98b33e02ff93fb65ecd3ba48ab409e2dcf92454")]
 [assembly: System.Reflection.AssemblyProductAttribute("Super Group 8-s Game 1 the movie the game Remastered and Knuckles X TEKKEN Featur" +
     "ing Dante From Devil May Cry")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Super Group 8-s Game 1 the movie the game Remastered and Knuckles X TEKKEN Featur" +
--- FallingPlatform.gd
+++ FallingPlatform.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
 var is_falling = false
+var counter = 0
 
 func _physics_process(delta):
+	if counter > 50:
+		queue_free()
 	if is_falling:
 		position.y += delta * 400
+		counter += 1
 
 func _on_area_2d_body_entered(body: CharacterBody2D):
 	if body.name == 'Player':
